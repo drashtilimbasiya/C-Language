12585eb4627d6004f473aa14e282235036146d07
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-05-04 12:47:12 +0530

    Do while loop
    Do while loop

---
+++ do-while loop 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        int j = 1;
+
+        do
+        {
+            printf("* ");
+            j++;
+        } while(j <= i);
+
+        printf("\n");
+        i++;
+    } while(i <= 5);
+
+    return 0;
+}
