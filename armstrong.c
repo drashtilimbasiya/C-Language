7985deea7ec259218fb753517737bf11c63e7c78
drashtilimbasiya <drashtidhirubhailimbasiya@gmail.com>
2026-05-04 21:03:08 +0530

    Armstrong number
    Armstrong number using while loop

---
+++ armstrong.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    //using while loop 
+    int n, i, sum = 0, temp, remainder;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    temp = n;
+    while (temp != 0)
+    {
+        remainder = temp % 10;
+        sum += remainder * remainder * remainder;
+        temp /= 10;
+    }
+    if (sum == n)
+        printf("%d is an Armstrong number.", n);
+    else
+        printf("%d is not an Armstrong number.", n);
+    return 0;
+}
